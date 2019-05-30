baa5119f1bcb22cd8b545d754c5ef7167b03298c
ndnguyen07 <github email address>
2019-05-30 14:41:39 -0700

    add authentication and login, with gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
 	has_many :characters
+	validates :username, :uniqueness => true
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_200522) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
