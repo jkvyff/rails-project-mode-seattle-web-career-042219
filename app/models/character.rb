c47dc9c2b9fd7911ae58d20873ae7b96da785695
github username <github email address>
2019-05-30 18:09:33 -0700

    Made stacking items, and more characters to interact with.

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,8 @@ class Character < ApplicationRecord
 	belongs_to :place
 	has_many :characters_items
 	has_many :items, through: :characters_items
+
+	def num_item(item)
+		CharactersItem.all.where(character_id: self.id, item_id: item.id)[0][:amount]
+	end
 end
--- db/migrate/20190528200046_create_characters_items.rb
+++ db/migrate/20190528200046_create_characters_items.rb
@@ -3,6 +3,7 @@ class CreateCharactersItems < ActiveRecord::Migration[5.2]
     create_table :characters_items do |t|
       t.integer :character_id
       t.integer :item_id
+      t.integer :amount
 
       t.timestamps
     end
